0d16cf0b536d6ad3249b4a46ee35d41a6c19154e
KShreve <kshreve@gmail.com>
2016-02-15 17:11:48 +0000

    Starting on putting styles into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 var path    = require('path'),
     webpack = require('webpack'),
+    autoprefixer = require('autoprefixer'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    sassLoaders = [
+        'css-loader',
+        'postcss-loader',
+        'sass-loader'
+    ],
     config  = {
         entry: [
             'webpack-hot-middleware/client?reload=true',
@@ -12,7 +19,8 @@ var path    = require('path'),
         },
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoErrorsPlugin()
+            new webpack.NoErrorsPlugin(),
+            new ExtractTextPlugin("styles.css")
         ],
         watch: true,
         devtool: 'eval-source-map',
@@ -25,8 +33,25 @@ var path    = require('path'),
                     query: {
                         presets: ["react-hmre"]
                     }
+                },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!postcss')
+                },
+                {
+                    test: /\.scss/,
+                    loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
                 }
             ]
+        },
+        postcss: [
+            autoprefixer({
+                browsers: ['last 2 versions']
+            })
+        ],
+        resolve: {
+            extensions: ['', '.js', '.jsx', '.scss'],
+            modulesDirectories: ['js', 'node_modules']
         }
     };
 
