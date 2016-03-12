d6c7f3acd6144aad086d58aac82ca068205e29fb
Kevin Shreve <kshreve@gmail.com>
2016-03-12 14:32:29 -0500

    moving from deprecated minify css to cleancss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp      = require('gulp'),
-    sass      = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
-    rename    = require('gulp-rename'),
-    flatten   = require('gulp-flatten');
+var gulp     = require('gulp'),
+    sass     = require('gulp-sass'),
+    cleanCss = require('gulp-clean-css'),
+    rename   = require('gulp-rename'),
+    flatten  = require('gulp-flatten');
 
 gulp.task('move', ['styles'], function() {
     return gulp.src(['assets/**/*.*', 'assets/**.*'], {base: './'})
@@ -13,7 +13,7 @@ gulp.task('move', ['styles'], function() {
 gulp.task('styles', function() {
     return gulp.src('styles/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(minifyCss())
+        .pipe(cleanCss())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
 });
