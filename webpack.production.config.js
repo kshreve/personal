b1a1ef8dc90f075827433624771f1f7de4153d10
Kevin Shreve <kshreve@gmail.com>
2016-03-27 21:41:37 -0400

    Try again to make production build work.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,30 +1,30 @@
-var path    = require('path'),
+var path = require('path'),
     webpack = require('webpack');
 
 module.exports = {
-    entry: './js/client.jsx',
-    output: {
-        path: path.join(__dirname, '/dist/'),
+    entry:   './js/client.jsx',
+    output:  {
+        path:     path.join(__dirname, '/dist/'),
         filename: 'bundle.min.js'
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({ minimize: true })
     ],
-    module: {
+    module:  {
         loaders: [
             {
-                test: /\.jsx?$/,
+                test:    /\.jsx?$/,
                 include: path.join(__dirname, 'js'),
                 loaders: ['babel']
             },
             {
-                    test: /\.css$/,
-                    loaders: ['style', 'css', 'postcss']
-                },
-                {
-                    test: /\.scss/,
-                    loaders: ['style', 'css', 'postcss', 'sass']
-                }
+                test:    /\.css$/,
+                loaders: ['style', 'css', 'postcss']
+            },
+            {
+                test:    /\.scss/,
+                loaders: ['style', 'css', 'postcss', 'sass']
+            }
         ]
     }
 };
