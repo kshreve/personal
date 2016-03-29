531dd9c013a848fc139c8ba4a2e1fdb95fd9ac7d
Kevin Shreve <kshreve@gmail.com>
2016-03-28 20:20:44 -0400

    adding google analytics script

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.get('/*', routeCache.cacheSeconds(3600), (req, res, next) => {
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Kevin Shreve</title>
+        <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-75715107-1', 'auto');ga('send', 'pageview');</script>
         <link href="/styles.min.css" type="text/css" rel="stylesheet" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
