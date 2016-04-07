1cc90b4c962c799703d36de8873dda182612e075
Kevin Shreve <kshreve@gmail.com>
2016-04-07 10:23:49 -0400

    Putting experiments under /experiments

--- js/controls/Nav.jsx
+++ js/controls/Nav.jsx
@@ -2,46 +2,48 @@ import React from 'react';
 import {Link} from 'react-router';
 
 import BaseComponent from './BaseComponent.jsx';
-import Routes from './../routes.jsx';
+import Routes, {experimentPath} from './../routes.jsx';
 
 export default class Nav extends BaseComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            experiments: ''
+            experimentsContainerClass: ''
         };
     }
 
     openExperiments() {
-        if (this.state.experiments === '') {
+        if (this.state.experimentsContainerClass === '') {
             this.setState({
-                experiments: 'nav__experiments-container--active'
+                experimentsContainerClass: 'nav__experiments-container--active'
             });
         } else {
             this.setState({
-                experiments: ''
+                experimentsContainerClass: ''
             });
         }
     }
 
     render() {
-        let navRoutes = Routes[0].props.children.filter((route) => route.props.nav),
-            experimentRoutes = Routes[0].props.children.filter((route) => route.props.experiment);
+        let {experimentsContainerClass} = this.state,
+            navRoutes = Routes[0].props.children.filter((route) => route.props.nav),
+            experimentsParent = Routes[0].props.children.filter((route) => route.props.path === experimentPath)[0],
+            experimentRoutes = experimentsParent.props.children.filter((route) => route.props.nav);
 
         return (
             <nav className="nav">
                 <ul className="flexzone container">
                     {
                         navRoutes.map((route) => {
-                            return <li key={route.key} className="list-item-unstyled nav__item-container"><Link className="nav__item" to={`/${route.key}`} activeClassName="nav__item--active">{route.props.title}</Link></li>;
+                            return <li key={route.key} className="list-item-unstyled nav__item-container"><Link className="nav__item" to={`/${route.props.path}`} activeClassName="nav__item--active">{route.props.title}</Link></li>;
                         })
                     }
-                    <li className="list-item-unstyled flexzone--reverse" onClick={this.openExperiments}><a>Experiments <span className={`icon ${this.state.experiments ? 'icon-circle-up' : 'icon-circle-down'}`}/></a>
-                        <ul className={`nav__experiments-container ${this.state.experiments}`}>
+                    <li className="list-item-unstyled flexzone--reverse" onClick={this.openExperiments}><a>Experiments <span className={`icon ${experimentsContainerClass ? 'icon-circle-up' : 'icon-circle-down'}`}/></a>
+                        <ul className={`nav__experiments-container ${experimentsContainerClass}`}>
                             {
                                 experimentRoutes.map((route) => {
-                                    return <li key={route.key} className="list-item-unstyled"><Link className="nav__item" to={`/${route.key}`} activeClassName="nav__item--active">{route.props.title}</Link></li>;
+                                    return <li key={route.key} className="list-item-unstyled"><Link className="nav__item" to={`/${experimentPath}/${route.props.path}`} activeClassName="nav__item--active">{route.props.title}</Link></li>;
                                 })
                             }
                         </ul>
--- js/routes.jsx
+++ js/routes.jsx
@@ -11,16 +11,20 @@ import NotFound from './views/NotFound.jsx';
 import WhiteLabelContainer from './views/whitelabel/WhiteLabelContainer.jsx';
 import Xkcd from './views/xkcd1335/Xkcd.jsx';
 
+export const experimentPath = 'experiment';
+
 export default [
     <Route key="/" path="/" component={AppContainer}>
         <IndexRoute title="About" component={About}/>
         <Route nav="true" key="about" path="about" title="About" component={About}/>
         <Route nav="true" key="blog" path="blog" title="Blog" component={Blog}/>
         <Route key="blog/:id" path="blog/:id" title="Blog Post" component={BlogPost}/>
-        <Route experiment="true" key="crankings" path="crankings" title="Combined Rankings" component={CrankingsContainer}/>
-        <Route key="crankingsData" path="crankingsData" title="Create Data - Combined Rankings" component={CrankingsDataImportContainer}/>
-        <Route experiment="true" key="whitelabel" path="whitelabel" title="White Labeling" component={WhiteLabelContainer}/>
-        <Route experiment="true" key="xkcd1335" path="xkcd1335" title="XKCD - 1335" component={Xkcd}/>
+        <Route key={experimentPath} path={experimentPath}>
+            <Route nav="true" key="crankings" path="crankings" title="Combined Rankings" component={CrankingsContainer}/>
+            <Route key="crankingsData" path="crankingsData" title="Create Data - Combined Rankings" component={CrankingsDataImportContainer}/>
+            <Route nav="true" key="whitelabel" path="whitelabel" title="White Labeling" component={WhiteLabelContainer}/>
+            <Route nav="true" key="xkcd1335" path="xkcd1335" title="XKCD - 1335" component={Xkcd}/>
+        </Route>
 
         <Route key="*" path="*" title="Not Found" component={NotFound}/>
     </Route>
