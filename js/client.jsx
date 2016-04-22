b6199030b46bc26f79cc6d381a38f9281ba1af04
Kevin Shreve <kshreve@gmail.com>
2016-04-21 21:32:02 -0400

    react-router is to blame.

--- js/client.jsx
+++ js/client.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Router, { browserHistory } from 'react-router';
+import Router from 'react-router';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import BrowserHistory from 'history/lib/createBrowserHistory';
 
 import routes from './routes.jsx';
 import configureStore from './redux/configureStore.jsx';
 
+const history = BrowserHistory();
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
 
 render((<Provider store={store}>
-        <Router history={browserHistory}>{routes}</Router>
+        <Router history={history}>{routes}</Router>
     </Provider>
 ), document.getElementById('route-mount'));
