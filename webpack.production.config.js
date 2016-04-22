267b0771cb6af276986e49339c7a7d6c88fdd531
Kevin Shreve <kshreve@gmail.com>
2016-04-21 20:57:01 -0400

    okay, something dumb, revert.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,9 +10,10 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             "process.env": {
-                NODE_ENV: JSON.stringify("development")
+                NODE_ENV: JSON.stringify("production")
             }
-        })
+        }),
+        new webpack.optimize.UglifyJsPlugin({ minimize: true }),
     ],
     module:  {
         loaders: [
