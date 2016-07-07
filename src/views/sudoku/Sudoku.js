9258eebe85e46c7d079730404215e18bf0778a0f
Kevin Shreve <kshreve@gmail.com>
2016-07-07 18:26:06 -0400

    check validity and check sums

--- src/views/sudoku/Sudoku.js
+++ src/views/sudoku/Sudoku.js
@@ -8,39 +8,50 @@ export default class Sudoku extends BaseComponent {
     }
 
     componentDidMount () {
-        let { sudoku:{ initialBoard }, massageBoard } = this.props;
+        let { massageBoard } = this.props;
 
-        return massageBoard(initialBoard);
+        return massageBoard();
+    }
+
+    checkSum (squares) {
+        return squares.map(square => square.content).reduce((previous, current) => previous + current, 0) === 45;
     }
 
     checkRegion (square) {
-        return this.props.sudoku.board.filter((item) => item.region === square.region && item.content === square.content).length === 1;
+        let region = this.props.sudoku.board.filter((item) => item.region === square.region);
+        return this.checkSum(region) && region.filter((item) => item.content === square.content).length === 1;
     }
 
     checkColumn (square) {
-        return this.props.sudoku.board.filter((item) => item.column === square.column && item.content === square.content).length === 1;
+        let column = this.props.sudoku.board.filter((item) => item.column === square.column);
+        return this.checkSum(column) && column.filter((item) => item.content === square.content).length === 1;
     }
 
     checkRow (square) {
-        return this.props.sudoku.board.filter((item) => item.row === square.row && item.content === square.content).length === 1;
+        let row = this.props.sudoku.board.filter((item) => item.row === square.row);
+        return this.checkSum(row) && row.filter((item) => item.content === square.content).length === 1;
     }
 
     checkValidity (square) {
-        let valid = this.checkRow(square) && this.checkColumn(square) && this.checkRegion(square);
-        console.log(valid);
-        return valid;
+        return this.checkRow(square) && this.checkColumn(square) && this.checkRegion(square);
+    }
+
+    checkWholePuzzle () {
+        let { sudoku: { board } } = this.props;
+
+        return board.map((square) => !this.checkValidity(square) && square).filter((valid) => valid).length === 0;
     }
 
     render () {
         let { sudoku:{ board } } = this.props,
             squares = board.map((square, i) => <div className="sudoku__square" key={i} onClick={() => this.checkValidity(square)}>{square.content}</div>);
 
-
         return (
             <div className="sudoku">
                 <div className="sudoku__board">
                     {squares}
                 </div>
+                <div>{this.checkWholePuzzle() ? `Valid` : `Invalid`}</div>
             </div>
         );
     }
--- src/views/sudoku/SudokuContainer.js
+++ src/views/sudoku/SudokuContainer.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
 
 import Sudoku from './Sudoku';
-import { massageBoard } from './../../redux/ducks/sudoku';
-
+import { massageBoard, isBoardValid } from './../../redux/ducks/sudoku';
 
 export default connect(
     (state) => ({
         sudoku: state.sudoku
     }),
     (dispatch) => ({
-        massageBoard: (board) => dispatch(massageBoard(board))
+        massageBoard: () => dispatch(massageBoard()),
+        isBoardValid: () => dispatch(isBoardValid())
     })
 )(Sudoku);
