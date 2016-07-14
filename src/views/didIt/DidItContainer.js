192bf29ada4dcda533518b9bb78ef01e047cbe6c
Kevin Shreve <kshreve@gmail.com>
2016-07-14 14:15:08 -0400

    didit now rehydrating with redux-persist.

--- src/views/didIt/DidItContainer.js
+++ src/views/didIt/DidItContainer.js
@@ -1,15 +1,14 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import DidIt from './DidIt';
-import {getDidIt, setPersonId, postDidIt} from './../../redux/ducks/didIt';
+import { getDidIt, setPersonId, postDidIt } from './../../redux/ducks/didIt';
 
 export default connect(
     (state) => ({
         didIt: state.didIt
     }),
     (dispatch) => ({
-        setPersonId:    (person) => dispatch(setPersonId(person)),
-        getDidIt:       (id) => dispatch(getDidIt(id)),
+        setPersonId:    () => dispatch(setPersonId()),
         incrementDidIt: (person) => dispatch(postDidIt(Object.assign({}, person, {
             times: person.times + 1
         })))
