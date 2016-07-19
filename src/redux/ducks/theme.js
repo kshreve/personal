263af8077cdb17872369f20707cbb3c1e2c4ff76
Kevin Shreve <kshreve@gmail.com>
2016-07-19 09:05:54 -0400

    Theme can also stick around now thanks to redux-persist.

--- src/redux/ducks/theme.js
+++ src/redux/ducks/theme.js
@@ -1,5 +1,7 @@
-import {post} from './../../convenience/functions';
-import {CUSTOM_THEME} from './../constants/endpoints';
+import { REHYDRATE } from 'redux-persist/constants';
+
+import { post } from './../../convenience/functions';
+import { CUSTOM_THEME } from './../constants/endpoints';
 
 const CREATE_CUSTOM_THEME_REQUEST = "CREATE_CUSTOM_THEME_REQUEST";
 const CREATE_CUSTOM_THEME_SUCCESS = "CREATE_CUSTOM_THEME_SUCCESS";
@@ -11,6 +13,14 @@ const initialState = {
 
 export default (state = initialState, action = null) => {
     switch (action.type) {
+        case REHYDRATE: {
+            let incoming = action.payload.theme;
+            if (incoming) {
+                return Object.assign({}, state, incoming);
+            }
+
+            return state;
+        }
         case CREATE_CUSTOM_THEME_SUCCESS:
             return Object.assign({}, state, {
                 name: action.payload.theme
